1503d5e0c93bf41d2db19197083c02e2d0b7c491
David Sanders <dpsanders@gmail.com>
2017-03-12 19:35:24 -0600

    Relax typing

--- src/variables.jl
+++ src/variables.jl
@@ -10,15 +10,45 @@ y = Variable(:y)
 
 
 immutable Sum <: Operation
-    var1::Variable
-    var2::Variable
+    var1
+    var2
 end
 
-Base.:+(x::Variable, y::Variable) = Sum(x, y)
+immutable Product <: Operation
+    var1
+    var2
+end
+
+immutable Power <: Operation
+    var1
+    var2
+end
+
+
+Base.:+(x, y) = Sum(x, y)
+Base.:*(x, y) = Product(x, y)
+Base.:^(x, y::Integer) = Power(x, y)
+Base.:^(x, y::Real) = Power(x, y)
+
+x^y
 
 x + y
 
-isa(x+y, Operation)
+x + (y*x)
+
+3x
+
+3x^2
+
+x
+
+@which x^2
+
+3 + 4
+
+x^10
+
+isa(x+y, Operation)d
 
 immutable Comparison
     operator::Symbol
